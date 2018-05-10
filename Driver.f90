4065093f47bb3a57e9c2feee2135afb9e980d996
baam333 <max.matysiak@hotmail.de>
2018-05-10 17:18:56 +0200

    klein Vieh

--- Driver.f90
+++ Driver.f90
@@ -3,18 +3,14 @@ program Driver
     implicit none
     REAL(KIND=RP) :: t=0.0_rp,tend=10.0_RP,CFL=0.4_RP,dt,a
     INTEGER,parameter :: n=0,nq=4
-    REAL(KIND=RP),Dimension(:,:,:,:,:,:,:),allocatable :: u,xyz
-    REAL(KIND=RP),Dimension(:),allocatable :: xi,xl
-    REAL(KIND=RP),Dimension(:,:),allocatable :: xin
+    REAL(KIND=RP),Dimension(:,:,:,:,:,:,:),allocatable :: u, usolution
     REAL(KIND=RP),DIMENSION(1:n+1,1:n+1)    ::D
     real(kind=RP), dimension(1:5) :: EOC,Fehler
     INTEGER :: l,m,o,k,i,j
-    allocate(u(1:Nq,1:nq,1:nq,1:n+1,1:n+1,1:n+1,1:5),xyz(1:Nq,1:nq,1:nq,1:n+1,1:n+1,1:n+1,1:3))
-    allocate(xi(1:n+1),xl(1:nq))
-    allocate(xin(1:n+1,1:nq))
+    allocate(u(1:Nq,1:nq,1:nq,1:n+1,1:n+1,1:n+1,1:5),usolution(1:Nq,1:nq,1:nq,1:n+1,1:n+1,1:n+1,1:5))
     Fehler=0.0_RP
     EOC=0.0_RP
-    call Vorbereiten(n,nq,D)
+    call Vorbereiten(n,nq,D,t)
     call Initialcondition(u)
     call lambdaMaxGlobal(u,a)
     dt=CFL/(3*a)*(dx/real(N+1,kind=RP))
@@ -33,5 +29,6 @@ program Driver
         !print*,u(1,1,1,1,:,:,1)
         t=t+dt
     end do
-                
+       call computeSolution(usolution) 
+        
 end program Driver
